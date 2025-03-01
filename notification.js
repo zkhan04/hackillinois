07722b62ad484a38baead9e5ded2f45b2bd530ab
Emily Masters <mastersemily518@gmail.com>
2025-03-01 13:54:30 -0600

    added requestPermission()

--- notification.js
+++ notification.js
@@ -3,31 +3,30 @@ const _opt = {
     type: "basic",
     title: "Lock in time!",
     message: `Looks like you're off task. It's time to lock in!`,
-    iconUrl: "url_to_small_icon"
+    // iconUrl: "url_to_small_icon"
   }
 
 const _id = "lock-in-notification";
 
-// const button = document.getElementById('enable-notifications');
-// button.addEventListener('click', askForNotificationPermission);
-
-// function askForNotificationPermission() {
-//     chrome.permissions.request(
-//       {
-//         permissions: ["notifications"],
-//       },
-//       function (granted) {
-//         if (granted) {
-//           showNotification(_id, _opt);
-//         } 
-//       }
-//     );
-
-// }
+function requestPermission() {
+  Notification.requestPermission()
+    .then((permission) => {
+      console.log('Promise resolved: ' + permission);
+      showPermission();
+    })
+    .catch((error) => {
+      console.log('Promise was rejected');
+      console.log(error);
+    });
+}
 
 async function showNotification(id, options) {
     if (Notification.permission === "granted") {
         await chrome.notifications.create(id, options);
+    } else if (Notification.permission === "default") {
+        requestPermission();
+        if (Notification.permission === "granted") {
+            await chrome.notifications.create(id, options);
+        }
     }
-    
 }
